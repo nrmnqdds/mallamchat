c5f1078c446077c015e02fe167ed4f7eb02efc21
nrmnqdds <quddusnuriman@gmail.com>
2024-07-27 20:37:25 +0800

    :green_heart: fix deployment

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,27 +1,29 @@
 import { auth } from "@/lib/auth";
 import { cn } from "@/lib/utils";
+import { unstable_noStore } from "next/cache";
 import { Space_Grotesk } from "next/font/google";
 import { redirect } from "next/navigation";
 
 const sp = Space_Grotesk({ display: "swap", subsets: ["latin"] });
 
 const ProtectedLayout = async ({ children }: { children: React.ReactNode }) => {
-  const session = await auth();
+	unstable_noStore();
+	const session = await auth();
 
-  if (!session) {
-    return redirect("/");
-  }
+	if (!session) {
+		return redirect("/");
+	}
 
-  return (
-    <main
-      className={cn(
-        "min-h-screen w-full bg-background flex-1 py-24",
-        sp.className,
-      )}
-    >
-      {children}
-    </main>
-  );
+	return (
+		<main
+			className={cn(
+				"min-h-screen w-full bg-background flex-1 py-24",
+				sp.className,
+			)}
+		>
+			{children}
+		</main>
+	);
 };
 
 export default ProtectedLayout;
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,44 +3,42 @@ import { auth } from "@/lib/auth";
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { SessionProvider } from "next-auth/react";
-import { unstable_noStore as noStore } from "next/cache";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Tatabahasaly",
-  description:
-    "Kecerdasan buatan bagi memastikan tatabahasa yang betul dalam penulisan",
+	title: "Tatabahasaly",
+	description:
+		"Kecerdasan buatan bagi memastikan tatabahasa yang betul dalam penulisan",
 };
 
 export default async function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  noStore();
-  const session = await auth();
+	const session = await auth();
 
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(
-          inter.className,
-          "min-h-screen bg-background antialiased",
-        )}
-      >
-        <SessionProvider session={session}>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider>
-        </SessionProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<body
+				className={cn(
+					inter.className,
+					"min-h-screen bg-background antialiased",
+				)}
+			>
+				<SessionProvider session={session}>
+					<ThemeProvider
+						attribute="class"
+						defaultTheme="dark"
+						disableTransitionOnChange
+					>
+						{children}
+					</ThemeProvider>
+				</SessionProvider>
+			</body>
+		</html>
+	);
 }
