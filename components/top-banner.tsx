a1bd3ce8e0b3130136b7d82dcaf1050b3f8bc055
nrmnqdds <quddusnuriman@gmail.com>
2024-08-01 03:39:35 +0800

    :lipstick: update banner

--- components/top-banner.tsx
+++ components/top-banner.tsx
@@ -1,14 +1,29 @@
-import React from "react";
+"use client";
+
+import Link from "next/link";
 
 const TopBanner = () => {
-  return (
-    <div className="w-full bg-blue-500 p-2 flex items-center justify-center">
-      <p className="font-bold">
-        🚧 Still in active rapid development. Please expect bugs and missing
-        features. 🚧
-      </p>
-    </div>
-  );
+	return (
+		<div className="w-full bg-blue-500 p-2 flex items-center justify-center">
+			{/* <p>{window.location.toString()}</p> */}
+			{window.location.toString().includes("localhost") && (
+				<p className="font-bold">
+					🚧 Do visit our new website at{" "}
+					<Link
+						href="https://mallam.chat"
+						target="_blank"
+						className="text-yellow-300 hover:underline"
+					>
+						mallam.chat
+					</Link>{" "}
+				</p>
+			)}
+			<p className="font-bold">
+				🚧 Still in active rapid development. Please expect bugs and missing
+				features. 🚧
+			</p>
+		</div>
+	);
 };
 
 export default TopBanner;
