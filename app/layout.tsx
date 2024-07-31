1bffaca4ce80e04d777e6e2b4680d1a902e98547
nrmnqdds <quddusnuriman@gmail.com>
2024-08-01 00:55:22 +0800

    :lipstick: add banner

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,43 +7,45 @@ import type { Metadata } from "next";
 import { SessionProvider } from "next-auth/react";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import TopBanner from "@/components/top-banner";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "MaLLaM Chat",
-	description:
-		"Kecerdasan buatan yang boleh membantu menjawab soalan pengguna.",
+  title: "MaLLaM Chat",
+  description:
+    "Kecerdasan buatan yang boleh membantu menjawab soalan pengguna.",
 };
 
 export default async function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	const session = await auth();
+  const session = await auth();
 
-	return (
-		<html lang="en" suppressHydrationWarning>
-			<body
-				className={cn(
-					inter.className,
-					"min-h-screen bg-background antialiased",
-				)}
-			>
-				<QueryProvider>
-					<SessionProvider session={session}>
-						<ThemeProvider
-							attribute="class"
-							defaultTheme="dark"
-							disableTransitionOnChange
-						>
-							{children}
-							<Toaster />
-						</ThemeProvider>
-					</SessionProvider>
-				</QueryProvider>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          inter.className,
+          "min-h-screen bg-background antialiased",
+        )}
+      >
+        <QueryProvider>
+          <SessionProvider session={session}>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="dark"
+              disableTransitionOnChange
+            >
+              <TopBanner />
+              {children}
+              <Toaster />
+            </ThemeProvider>
+          </SessionProvider>
+        </QueryProvider>
+      </body>
+    </html>
+  );
 }
---
+++ components/top-banner.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const TopBanner = () => {
+  return (
+    <div className="w-full bg-blue-500 p-2 flex items-center justify-center">
+      <p className="font-bold">
+        🚧 Still in active rapid development. Please expect bugs and missing
+        features. 🚧
+      </p>
+    </div>
+  );
+};
+
+export default TopBanner;
